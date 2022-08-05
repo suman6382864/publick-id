1562bf528ecc72eec6d6eb2713cbe2734e92a5e1
suman6382864 <108501455+suman6382864@users.noreply.github.com>
2022-08-05 20:48:49 +0600

    Update SUMAN.py

--- SUMAN.py
+++ SUMAN.py
@@ -18,16 +18,16 @@ except ImportError:
 print('\n\x1b[1;37m[•] Checking Update...');time.sleep(0.5)
 os.system('git pull')
 def Run():
-        bit = platform.architecture()[0]
-        if bit == '64bit':
-            print("\x1b[1;92m[•] Congratulations ! Your Device Support this Tools")
-            print('[•] Join Over Facebook Group First')
-            os.system('xdg-open https://facebook.com/groups/351076900316263/')
+        
+2
+
+
+
             import RNDM
         elif bit == '32bit':
             print("\n\x1b[1;92m[•] Congratulations ! Your Device Support this Tools")
             print('[•] Join Over Facebook Group First')
-            os.system('xdg-open https://facebook.com/groups/351076900316263/')
+            os.system('xdg-open https://www.facebook.com/groups/994845054533001/')
             import RNDM32
         else:
             exit('\033[1;31m[×] Connection Error')
